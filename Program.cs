6443dee53ac1a87b7728c4cdd357293a2d514aa6
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2019-10-09 17:47:52 +0300

    reminder
    Start of the project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reminder
+{
+    class Program
+    {
+
+        public static void remind(int interval)
+        {
+            DateTime endTime = new DateTime().AddSeconds(interval);
+            
+        }
+        static void Main(string[] args)
+        {
+            string taskName = "task1";
+            int interval = 3;
+            Task task1 = new Task(taskName, interval);
+
+            String input = Console.ReadLine();
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Task
+{
+    string name;
+    int interval;
+
+    public Task(string name, int interval)
+    {
+        this.name       = name;
+        this.interval   = interval;
+    }
+}
